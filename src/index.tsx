3dfa15500130dda9cf064c1b785d2f40a22f87e0
honmakousuke <kou4321ske@gmail.com>
2022-01-27 20:53:24 +0900

    hooks追加

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,19 @@
 import * as React from 'react';
 import { render } from 'react-dom';
+import styled from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
+import { Editor } from './pages/editor';
 
-const Main = (<h1>Markdown Editor</h1>);
+const GlobalStyle = createGlobalStyle`
+  body * {
+    box-sizing: border-box;
+  }
+`
+const Main = (
+  <React.Fragment>
+    <GlobalStyle />
+    <Editor />
+  </React.Fragment>
+);
 
-render(Main, document.getElementById('app'));
+render(Main, document.getElementById("app"));
