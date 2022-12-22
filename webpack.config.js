28e0587f40e3e60fc42b26a253542defd200cd65
kosukehomma <kou4321ske@gmail.com>
2022-12-22 20:50:13 +0900

    markdown button add

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
+const { getTsBuildInfoEmitOutputFilePath } = require('typescript');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.tsx',
   module: {
     rules: [
@@ -18,5 +20,15 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
     publicPath: 'dist/',
-  }
+  },
+  devServer: {
+    port: 3003,
+    static: {
+      directory: path.join(__dirname, '/'),
+    },
+    hot: true,
+    open: true,
+    watchFiles: ['dist'],
+  },
+  target: ['web', 'es5']
 }
