f129796c75808e136a07650841a5fb3a9c8173de
kosukehomma <kou4321ske@gmail.com>
2022-12-23 16:31:08 +0900

    worker-loader導入

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,17 @@
 import * as React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import styled from 'styled-components';
 import { createGlobalStyle } from 'styled-components';
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 import { Editor } from './pages/editor';
+import { History } from './pages/history';
+import { useStateWithStorage } from './hooks/use_state_with_storage';
+import { render } from 'react-dom';
 
 const GlobalStyle = createGlobalStyle`
   body * {
@@ -10,11 +19,40 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-const Main = (
-  <>
-    <GlobalStyle />
-    <Editor />
-  </>
-);
+const StorageKey = '/editor:text';
 
-render(Main, document.getElementById('app'));
+const Main: React.FC = () => {
+  const [text, setText] = useStateWithStorage('', StorageKey);
+
+  return (
+    <>
+      <GlobalStyle />
+      <Router>
+        <Routes>
+          <Route
+            path="/editor"
+            element={
+              <Editor text={text} setText={setText} />
+            }
+          />
+          <Route
+            path="/history"
+            element={
+              <History setText={setText} />
+            }
+          />
+          <Route
+            path="*"
+            element={
+              <Navigate to="/editor" />
+            }
+          />
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+root.render(<Main />)
--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -4,26 +4,13 @@ import { useStateWithStorage } from '../hooks/use_state_with_storage';
 import ReactMarkdown from 'react-markdown';
 import { putMemo } from '../indexeddb/memos';
 import { Button } from '../components/button';
+import { SaveModal } from '../components/save_modal';
+import { Link } from 'react-router-dom';
+import { Header } from '../components/header';
+import TestWorker from 'worker-loader!../worker/test.ts';
 
-const Header = styled.header`
-  align-content: center;
-  display: flex;
-  font-size: 1.5rem;
-  height: 2rem;
-  justify-content: space-between;
-  left: 0;
-  line-height: 2rem;
-  padding: 0.5rem 1rem;
-  position: fixed;
-  right: 0;
-  top: 0;
-`;
-
-const HeaderControl = styled.div`
-  height: 2rem;
-  display: flex;
-  align-content: center;
-`;
+const testWorker = new TestWorker();
+const { useState, useEffect } = React;
 
 const Wrapper = styled.div`
   bottom: 0;
@@ -31,6 +18,13 @@ const Wrapper = styled.div`
   position: fixed;
   right: 0;
   top: 3rem;
+`;
+
+const HeaderArea = styled.div`
+  position: fixed;
+  right: 0;
+  top: 0;
+  left: 0;
 `;
 
 const TextArea = styled.textarea`
@@ -56,25 +50,37 @@ const Preview = styled.div`
   width: 50vw;
 `;
 
-const StorageKey = 'pages/editor:text';
+interface Props {
+  text: string;
+  setText: (text: string) => void;
+}
 
-export const Editor: React.FC = () => {
-  const [text, setText] = useStateWithStorage('', StorageKey);
+export const Editor: React.FC<Props> = (props) => {
+  const { text, setText } = props;
+  const [showModal, setShowModal] = useState(false);
 
-  const saveMemo = (): void => {
-    putMemo('TITLE', text)
-  }
+  useEffect(() => {
+    testWorker.onmessage = (event) => {
+      console.log('Main thread Received:', event.data);
+    }
+  }, []);
+
+  useEffect(() => {
+    testWorker.postMessage(text);
+  }, [text]);
 
   return (
     <>
-      <Header>
-        Markdown Editor
-        <HeaderControl>
-          <Button onClick={saveMemo}>
+      <HeaderArea>
+        <Header title="Markdown Editor">
+          <Button onClick={() => setShowModal(true)}>
             保存する
           </Button>
-        </HeaderControl>
-      </Header>
+          <Link to="/history">
+            履歴を見る
+          </Link>
+        </Header>
+      </HeaderArea>
       <Wrapper>
         <TextArea
           onChange={(event) => setText(event.target.value)}
@@ -84,6 +90,15 @@ export const Editor: React.FC = () => {
           <ReactMarkdown children={text} />
         </Preview>
       </Wrapper>
+      {showModal && (
+        <SaveModal
+          onSave={(title: string): void => {
+            putMemo(title, text)
+            setShowModal(false)
+          }}
+          onCancel={() => setShowModal(false)}
+        />
+      )}
     </>
   )
 };
